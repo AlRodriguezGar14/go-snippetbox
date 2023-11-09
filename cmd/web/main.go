10c76780b1eb3d5875e58c4ee7373110ea24f3c3
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-09 20:28:51 +0100

    improved the use of info/error logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,30 +4,41 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 type config struct {
-	addr      string
-	staticDir string
+	Addr      string
+	StaticDir string
 }
 
 func main() {
 	var cfg config
 
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP port addres")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static/", "Path to the static assets")
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP port Addres")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static/", "Path to the static assets")
 	// This reads the command line arg and adds it to the declared variable
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
-
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", cfg.addr)
-	err := http.ListenAndServe(cfg.addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     cfg.Addr,
+		ErrorLog: errLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", cfg.Addr)
+	err := srv.ListenAndServe()
+	errLog.Fatal(err)
 }
